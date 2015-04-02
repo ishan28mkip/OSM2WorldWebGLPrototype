56de17b7acdd98fb125693af9da3369916b950bc
Ishan <ishan28mkip@gmail.com>
2015-04-03 01:33:42 +0530

    Version 0.2

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # OSM2WorldWebGLPrototype 
 A prototype for the WebGL frontend of OSM2World.
+
+Current Version 0.2
+====================
+
+Version 0.2
+------------
+* The browser now loads the .obj and the associated .mtl file together.
+* Ability to Pan/Drag/Zoom added.
+* Fixed background color.
+
+Version 0.1
+------------
+* The code currently just establishes a 3D framework wherein .obj files can be loaded. 
+* Texture support is currently missing. Due to the default color of the maps created by OSM2World being dark blue, visibility is very poor, texture support would be implemented in the next update.
+* Prototype prompts for 0 map version, 1 for model version.
--- libs/MTLLoader.js
+++ libs/MTLLoader.js
@@ -16,18 +16,56 @@ THREE.MTLLoader.prototype = {
 
 	constructor: THREE.MTLLoader,
 
-	load: function ( url, onLoad, onProgress, onError ) {
+	/**
+	 * Loads a MTL file
+	 *
+	 * Loading progress is indicated by the following events:
+	 *   "load" event (successful loading): type = 'load', content = THREE.MTLLoader.MaterialCreator
+	 *   "error" event (error loading): type = 'load', message
+	 *   "progress" event (progress loading): type = 'progress', loaded, total
+	 *
+	 * @param url - location of MTL file
+	 */
+	load: function( url ) {
 
 		var scope = this;
+		var xhr = new XMLHttpRequest();
 
-		var loader = new THREE.XHRLoader();
-		loader.setCrossOrigin( this.crossOrigin );
-		loader.load( url, function ( text ) {
+		function onloaded( event ) {
 
-			onLoad( scope.parse( text ) );
+			if ( event.target.status === 200 || event.target.status === 0 ) {
 
-		}, onProgress, onError );
+				var materialCreator = scope.parse( event.target.responseText );
 
+				// Notify caller, that I'm done
+
+				scope.dispatchEvent( { type: 'load', content: materialCreator } );
+
+			} else {
+
+				scope.dispatchEvent( { type: 'error', message: 'Couldn\'t load URL [' + url + ']',
+					response: event.target.responseText } );
+
+			}
+
+		}
+
+		xhr.addEventListener( 'load', onloaded, false );
+
+		xhr.addEventListener( 'progress', function ( event ) {
+
+			scope.dispatchEvent( { type: 'progress', loaded: event.loaded, total: event.total } );
+
+		}, false );
+
+		xhr.addEventListener( 'error', function () {
+
+			scope.dispatchEvent( { type: 'error', message: 'Couldn\'t load URL [' + url + ']' } );
+
+		}, false );
+
+		xhr.open( 'GET', url, true );
+		xhr.send( null );
 	},
 
 	/**
@@ -35,14 +73,14 @@ THREE.MTLLoader.prototype = {
 	 * @param text - Content of MTL file
 	 * @return {THREE.MTLLoader.MaterialCreator}
 	 */
-	parse: function ( text ) {
+	parse: function( text ) {
 
 		var lines = text.split( "\n" );
 		var info = {};
 		var delimiter_pattern = /\s+/;
 		var materialsInfo = {};
 
-		for ( var i = 0; i < lines.length; i ++ ) {
+			for ( var i = 0; i < lines.length; i ++ ) {
 
 			var line = lines[ i ];
 			line = line.trim();
@@ -56,7 +94,7 @@ THREE.MTLLoader.prototype = {
 
 			var pos = line.indexOf( ' ' );
 
-			var key = ( pos >= 0 ) ? line.substring( 0, pos ) : line;
+			var key = ( pos >= 0 ) ? line.substring( 0, pos) : line;
 			key = key.toLowerCase();
 
 			var value = ( pos >= 0 ) ? line.substring( pos + 1 ) : "";
@@ -87,7 +125,6 @@ THREE.MTLLoader.prototype = {
 		}
 
 		var materialCreator = new THREE.MTLLoader.MaterialCreator( this.baseUrl, this.options );
-		materialCreator.crossOrigin = this.crossOrigin
 		materialCreator.setMaterials( materialsInfo );
 		return materialCreator;
 
@@ -121,8 +158,8 @@ THREE.MTLLoader.MaterialCreator = function( baseUrl, options ) {
 	this.materialsArray = [];
 	this.nameLookup = {};
 
-	this.side = ( this.options && this.options.side ) ? this.options.side : THREE.FrontSide;
-	this.wrap = ( this.options && this.options.wrap ) ? this.options.wrap : THREE.RepeatWrapping;
+	this.side = ( this.options && this.options.side )? this.options.side: THREE.FrontSide;
+	this.wrap = ( this.options && this.options.wrap )? this.options.wrap: THREE.RepeatWrapping;
 
 };
 
@@ -289,7 +326,7 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 					// Diffuse color (color under white light) using RGB values
 
-					params[ 'diffuse' ] = new THREE.Color().fromArray( value );
+					params[ 'diffuse' ] = new THREE.Color().setRGB( value[0], value[1], value[2] );
 
 					break;
 
@@ -297,12 +334,14 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 					// Ambient color (color under shadow) using RGB values
 
+					params[ 'ambient' ] = new THREE.Color().setRGB( value[0], value[1], value[2] );
+
 					break;
 
 				case 'ks':
 
 					// Specular color (color when light is reflected from shiny surface) using RGB values
-					params[ 'specular' ] = new THREE.Color().fromArray( value );
+					params[ 'specular' ] = new THREE.Color().setRGB( value[0], value[1], value[2] );
 
 					break;
 
@@ -340,19 +379,6 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 					break;
 
-				case 'map_bump':
-				case 'bump':
-
-					// Bump texture map
-
-					if ( params[ 'bumpMap' ] ) break; // Avoid loading twice.
-
-					params[ 'bumpMap' ] = this.loadTexture( this.baseUrl + value );
-					params[ 'bumpMap' ].wrapS = this.wrap;
-					params[ 'bumpMap' ].wrapT = this.wrap;
-
-					break;
-
 				default:
 					break;
 
@@ -362,6 +388,7 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 		if ( params[ 'diffuse' ] ) {
 
+			if ( !params[ 'ambient' ]) params[ 'ambient' ] = params[ 'diffuse' ];
 			params[ 'color' ] = params[ 'diffuse' ];
 
 		}
@@ -374,18 +401,18 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 	loadTexture: function ( url, mapping, onLoad, onError ) {
 
-		var texture;
-		var loader = THREE.Loader.Handlers.get( url );
+		var isCompressed = /\.dds$/i.test( url );
 
-		if ( loader !== null ) {
+		if ( isCompressed ) {
 
-			texture = loader.load( url, onLoad );
+			var texture = THREE.ImageUtils.loadCompressedTexture( url, mapping, onLoad, onError );
 
 		} else {
 
-			texture = new THREE.Texture();
+			var image = new Image();
+			var texture = new THREE.Texture( image, mapping );
 
-			loader = new THREE.ImageLoader();
+			var loader = new THREE.ImageLoader();
 			loader.crossOrigin = this.crossOrigin;
 			loader.load( url, function ( image ) {
 
@@ -398,8 +425,6 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 		}
 
-		if ( mapping !== undefined ) texture.mapping = mapping;
-
 		return texture;
 
 	}
@@ -408,7 +433,7 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 
 THREE.MTLLoader.ensurePowerOfTwo_ = function ( image ) {
 
-	if ( ! THREE.Math.isPowerOfTwo( image.width ) || ! THREE.Math.isPowerOfTwo( image.height ) ) {
+	if ( ! THREE.MTLLoader.isPowerOfTwo_( image.width ) || ! THREE.MTLLoader.isPowerOfTwo_( image.height ) ) {
 
 		var canvas = document.createElement( "canvas" );
 		canvas.width = THREE.MTLLoader.nextHighestPowerOfTwo_( image.width );
@@ -424,9 +449,15 @@ THREE.MTLLoader.ensurePowerOfTwo_ = function ( image ) {
 
 };
 
+THREE.MTLLoader.isPowerOfTwo_ = function ( x ) {
+
+	return ( x & ( x - 1 ) ) === 0;
+
+};
+
 THREE.MTLLoader.nextHighestPowerOfTwo_ = function( x ) {
 
-	-- x;
+	--x;
 
 	for ( var i = 1; i < 32; i <<= 1 ) {
 
